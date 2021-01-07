c64dc30c4d64105948d9322bd7e85283ef756d9b
loquitowen <mosquito.ix@qq.com>
2021-01-07 10:52:35 +0800

    Initial commit

---
+++ src/main/java/cn/gov/gxzf/dnr/xxzx/loquito/LoquitoApplication.java
@@ -0,0 +1,13 @@
+package cn.gov.gxzf.dnr.xxzx.loquito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoquitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoquitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/gov/gxzf/dnr/xxzx/loquito/LoquitoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gov.gxzf.dnr.xxzx.loquito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoquitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
